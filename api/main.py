1b655bc4f6bd98be14270ac30b220f344415a39f
Sean Saturley <ssaturle@charlotte.edu>
2024-11-02 18:55:41 -0400

    feat(api): add Resources to FastAPI

---
+++ api/main.py
@@ -0,0 +1,83 @@
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
+
+from .models import models, schemas
+from .controllers import orders, resources
+from .dependencies.database import engine, get_db
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ORDER API CODE
+@app.post("/orders/", response_model=schemas.Order, tags=["Orders"])
+def create_order(order: schemas.OrderCreate, db: Session = Depends(get_db)):
+    return orders.create(db=db, order=order)
+
+@app.get("/orders/", response_model=list[schemas.Order], tags=["Orders"])
+def read_orders(db: Session = Depends(get_db)):
+    return orders.read_all(db)
+
+@app.get("/orders/{order_id}", response_model=schemas.Order, tags=["Orders"])
+def read_one_order(order_id: int, db: Session = Depends(get_db)):
+    order = orders.read_one(db, order_id=order_id)
+    if order is None:
+        raise HTTPException(status_code=404, detail="Order not found")
+    return order
+
+@app.put("/orders/{order_id}", response_model=schemas.Order, tags=["Orders"])
+def update_one_order(order_id: int, order: schemas.OrderUpdate, db: Session = Depends(get_db)):
+    order_db = orders.read_one(db, order_id=order_id)
+    if order_db is None:
+        raise HTTPException(status_code=404, detail="Order not found")
+    return orders.update(db=db, order=order, order_id=order_id)
+
+@app.delete("/orders/{order_id}", tags=["Orders"])
+def delete_one_order(order_id: int, db: Session = Depends(get_db)):
+    order = orders.read_one(db, order_id=order_id)
+    if order is None:
+        raise HTTPException(status_code=404, detail="Order not found")
+    return orders.delete(db=db, order_id=order_id)
+
+
+
+# RESOURCE API CODE
+@app.post("/resources/", response_model=schemas.Resource, tags=["Resources"])
+def create_resource(resource: schemas.ResourceCreate, db: Session = Depends(get_db)):
+    return resources.create(db=db, resource=resource)
+
+@app.get("/resources/", response_model=list[schemas.Resource], tags=["Resources"])
+def read_resources(db: Session = Depends(get_db)):
+    return resources.read_all(db)
+
+@app.get("/resources/{resource_id}", response_model=schemas.Resource, tags=["Resources"])
+def read_one_resource(resource_id: int, db: Session = Depends(get_db)):
+    resource = resources.read_one(db, resource_id=resource_id)
+    if resource is None:
+        raise HTTPException(status_code=404, detail="Order not found")
+    return resource
+
+@app.put("/resources/{resource_id}", response_model=schemas.Resource, tags=["Resources"])
+def update_one_resource(resource_id: int, resource: schemas.ResourceUpdate, db: Session = Depends(get_db)):
+    resource_db = resources.read_one(db, resource_id=resource_id)
+    if resource_db is None:
+        raise HTTPException(status_code=404, detail="Resource not found")
+    return resources.update(db=db, resource=resource, resource_id=resource_id)
+
+@app.delete("/resources/{resource_id}", tags=["Resources"])
+def delete_one_resource(resource_id: int, db: Session = Depends(get_db)):
+    resource = resources.read_one(db, resource_id=resource_id)
+    if resource is None:
+        raise HTTPException(status_code=404, detail="Order not found")
+    return resources.delete(db=db, resource_id=resource_id)
